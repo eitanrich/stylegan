fcfe771c6e5adb83422fec296a5156055392f35a
eitanrich <eitan.richardson@gmail.com>
2019-07-23 11:31:05 +0300

    Adding output message to assert statement

--- training/dataset.py
+++ training/dataset.py
@@ -67,7 +67,7 @@ class TFRecordDataset:
         self._cur_lod           = -1
 
         # List tfrecords files and inspect their shapes.
-        assert os.path.isdir(self.tfrecord_dir)
+        assert os.path.isdir(self.tfrecord_dir), self.tfrecord_dir + ' not found'
         tfr_files = sorted(glob.glob(os.path.join(self.tfrecord_dir, '*.tfrecords')))
         assert len(tfr_files) >= 1
         tfr_shapes = []
